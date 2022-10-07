3ecbaf24f95868501a8af89cd02213f211b7fc4b
Bogdan <c0pland@icloud.com>
2022-10-07 12:52:38 +0300

    010 - add animation and blur effect

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -102,9 +102,15 @@ struct ContentView: View {
 					.padding(.vertical, 0)
 					.frame(maxWidth: 640)
 				}
+				.blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+				.transition(.move(edge: .bottom))
+				.animation(.easeOut(duration: 0.5))
 					// MARK: New task item
 				if showNewTaskItem {
-					BlankView()
+					BlankView(
+						backgroundColor: isDarkMode ? .black : .gray,
+						backgroundOpacity: isDarkMode ? 0.3 : 0.5
+					)
 						.onTapGesture {
 							withAnimation {
 								showNewTaskItem = false
